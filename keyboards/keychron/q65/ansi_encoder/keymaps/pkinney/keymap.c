8d4da6462f68fd16811f7723114da42a457aa692
Powell Kinney <628952+pkinney@users.noreply.github.com>
2023-02-17 06:54:36 -0600

    Adds hyper keys

--- keyboards/keychron/q65/ansi_encoder/keymaps/pkinney/keymap.c
+++ keyboards/keychron/q65/ansi_encoder/keymaps/pkinney/keymap.c
@@ -31,10 +31,10 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_73(
         KC_MUTE, KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_DEL,
-        _______, KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_HOME,
-        _______, CTL_T(KC_ESC), KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           KC_PGUP,
-        _______, KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,   KC_PGDN,
-        _______, KC_LCTL, KC_LOPT,  KC_LCMD,                             KC_SPC,                             KC_MEH,  KC_HYPR, MO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT),
+        HYPR(KC_F1), KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          HYPR(KC_F6),
+        HYPR(KC_F2), CTL_T(KC_ESC), KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,           HYPR(KC_F7),
+        HYPR(KC_F3), KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,   HYPR(KC_F8),
+        HYPR(KC_F4), KC_LCTL, KC_LOPT,  KC_LCMD,                             KC_SPC,                             KC_MEH,  KC_HYPR, MO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_73(
         KC_MUTE, KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_DEL,
@@ -56,14 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          _______,
         _______, _______,           _______,  _______, _______, _______, _______, NK_TOGG, _______, _______, _______,  _______,            _______, _______, _______,
         _______, _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
-
-    [_FN3] = LAYOUT_ansi_73(
-        _______, KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        _______, RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          _______,
-        _______, _______,           _______,  _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______, _______, _______,
-        _______, _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______)
-};
+   };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
@@ -71,6 +64,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_FN1] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [_FN2] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [_FN3] = { ENCODER_CCW_CW(_______, _______)}
 };
 #endif // ENCODER_MAP_ENABLE
